e0a977546f7bfe41f8290a806fb6c77ede8c0d0b
Ernst-Jan <ernst-janbakker@live.nl>
2018-02-12 22:36:58 +0100

    test

--- Week1/7.1.1.php
+++ Week1/7.1.1.php
@@ -5,3 +5,49 @@
  * Date: 12-2-2018
  * Time: 21:12
  */
+
+    $gewicht1 = 90;
+    $lengte1 = 1.80;
+
+
+
+    $opdracht1 = $gewicht1 / ($lengte1 * $lengte1);
+
+    echo "opdracht 1: ";
+
+    if($opdracht1 < 18) {
+        echo "Ondergewicht";
+    }
+    elseif($opdracht1 < 25){
+        echo "Normaal gewicht";
+    }
+    elseif($opdracht1 < 30){
+        echo "Overgewicht";
+    }
+    elseif($opdracht1 < 40){
+        echo "Ernstig overgewicht";
+    }
+
+    echo "<br>opdracht 2: ";
+
+
+
+
+    echo "<br>opdracht 3: ";
+
+
+
+
+    echo "<br>opdracht 4: ";
+
+
+
+
+
+
+
+
+
+
+
+?>
